52e22ab83905dba60e60c71320b1a09a314fb23a
Vishwas <VishwasDubey150@gmail.com>
2022-02-10 18:07:52 +0530

    Initial commit

---
+++ SpecVoiceAssistantIntergrated.py
@@ -0,0 +1,417 @@
+import os
+import sys
+import pyttsx3
+#text to speech
+import datetime
+import pyaudio
+import wikipedia
+import webbrowser
+import pyjokes
+from datetime import date
+import speech_recognition as sr
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice',voices[1].id)
+import cv2
+
+a=True
+b=True
+
+import mediapipe as mp
+
+import numpy as np
+
+import time
+
+import HandTrackingModule as htm
+
+import math
+
+from ctypes import cast, POINTER
+
+from comtypes import CLSCTX_ALL
+
+from pycaw.pycaw import AudioUtilities , IAudioEndpointVolume
+
+##################
+wCam,hCam=640,480
+#heigt and width of webcam window
+##################
+
+pTime=0
+#variables to calculate fps
+cTime=0
+
+ # main loop
+cap = cv2.VideoCapture(0)
+#to capture the webcam video feed
+
+cap.set(3,wCam)
+#setting width and the height
+cap.set(4,hCam)
+
+detector=htm.handDetector(detectionCon=0.4)
+
+devices=AudioUtilities.GetSpeakers()
+
+interface=devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL,None
+)
+
+volume=cast(interface,POINTER(IAudioEndpointVolume))
+#volume.GetMute()
+#volume.GetMasterVolumeLevel()
+vol_range=volume.GetVolumeRange()
+
+min_vol=vol_range[0]
+max_vol=vol_range[1]
+
+vol=0
+vol_bar=400
+vol_percentage=0
+
+tip_ids=[4,8,12,16,20]
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+def wishMe():
+    hour=int(datetime.datetime.now().hour)
+    if hour>5 and hour<12:
+        speak("Good mmorning sir , how may i help you")
+    elif hour>=12 and hour<16:
+        speak("Good afternoon sir, how may i help you")
+    elif hour>=16 and hour<20:
+        speak("Good evening sir, how may i help you")
+    else:
+        speak("hello sir, how may i help you")
+def takeCommand():
+    r=sr.Recognizer()
+    with sr.Microphone()as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio =r.listen(source)
+    try:
+        print("Recognizing...")
+        question=r.recognize_google(audio,language='en-in')
+        print(f"User said: {question}\n")
+    except Exception as e:
+        #print(e)
+        print("Please say it again")
+        return "None"
+    return question
+if __name__ == "__main__":
+    wishMe()
+    while True:
+        question = takeCommand().lower()
+        if 'who' in question:
+            speak('Searching Wikipedia...')
+            print('searching...')
+            question = question.replace("wikipedia", "")
+            results = wikipedia.summary(question, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+            speak("Do you think detail is wrong")
+            print("Do you think detail is wrong")
+            takeCommand()
+            if 'yes' in question:
+                speak("Sorry for inconvenience, please search on wikipedia yourself, here you go")
+                print("Sorry for inconvenience, please search on wikipedia yourself, here you go")
+                webbrowser.open("https://www.wikipedia.org/")
+            else:
+                speak("thank you for your feedback")
+                print("thank you for your feedback")
+
+
+        elif "finger" in question:
+            speak("launching finger gesture")
+            print("launching finger gesture")
+            while (b==True):
+
+                success, img = cap.read()  # reading the video footage
+
+                img = detector.findHands(img)
+                # find hands is a method of the class hand detector in the hand tracking module
+                # returns the image
+
+                lm_list = detector.findPosition(img, draw=False)
+                # also a method
+                # can draw on specefic points or on all points
+                # print(lm_list) #shows the position of all the points on the hand per in a list
+
+                if len(lm_list) != 0:  # when hand is detected
+
+                    fingers_open = []
+
+                    if lm_list[tip_ids[0]][1] < lm_list[tip_ids[0] - 1][1]:
+                        # extra for thumb
+
+                        fingers_open.append(0)
+                    else:
+                        fingers_open.append(1)
+
+                    for id in range(1, 5):
+
+                        # check if the point 8 is below the point 6 of the same finger
+                        if lm_list[tip_ids[id]][2] < lm_list[tip_ids[id] - 2][2]:
+                            # means finger open  #because of open cv pixels it is reverse
+                            fingers_open.append(1)
+                        else:
+
+                            fingers_open.append(0)
+
+                    print(fingers_open)
+
+                    total_fingers_open = fingers_open.count(1)
+
+                    cv2.rectangle(img, (20, 225), (170, 425),
+                                  (0, 0, 255), cv2.FILLED)
+
+                    cv2.putText(img, str(total_fingers_open), (70, 350),
+                                cv2.FONT_HERSHEY_COMPLEX, 3, (0, 255, 0), 3)
+
+                    if total_fingers_open==1:
+                        speak('opening google')
+                        print('opening google')
+                        webbrowser.open("www.google.com")
+                        b=False
+                    elif total_fingers_open==2:
+                        speak('opening youtube')
+                        print('opening youtube')
+                        webbrowser.open("youtube.com")
+                        b=False
+                    elif total_fingers_open==3:
+                        speak('opening music')
+                        print('opening music')
+                        webbrowser.open("https://music.youtube.com/")
+                        b=False
+                    elif total_fingers_open==4:
+                        webbrowser.open("https://mail.google.com/mail/u/1/#inbox")
+                        speak("opening gmail")
+                        print("opening gmail")
+                        b=False
+                    elif total_fingers_open==5:
+                        speak('opening whattsapp')
+                        print('opening whattsapp')
+                        webbrowser.open("https://web.whatsapp.com/")
+                        b=False
+
+                cTime = time.time()
+                # current time for fps calculation
+                fps = 1 / (cTime - pTime)
+                # fps calculation
+                pTime = cTime
+                # updating the value as the loop executes
+
+                # TEXT LOCATION                       #FONT SCALE     FONT THICKNESS
+                cv2.putText(img, f'FPS: {int(fps)}', (400, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+
+                cv2.imshow("FINGER COUNTER", img)
+                # the heading of the window showing the webcam video footage
+
+                cv2.waitKey(1)
+                # setting the delay
+
+        elif "volume" in question:
+            speak("launching volume gesture control")
+            print("launching volume geture control")
+            while (a==True):
+
+                success, img = cap.read()
+                # capture the video of webcam
+
+                img = detector.findHands(img)
+                lm_list = detector.findPosition(img, draw=False)
+
+                if len(lm_list) != 0:  # when hand is detected
+
+                    fingers_open = []
+
+                    if lm_list[tip_ids[0]][1] < lm_list[tip_ids[0] - 1][1]:
+                        # extra for thumb
+
+                        fingers_open.append(0)
+                    else:
+                        fingers_open.append(1)
+
+                    for id in range(1, 5):
+
+                        # check if the point 8 is below the point 6 of the same finger
+                        if lm_list[tip_ids[id]][2] < lm_list[tip_ids[id] - 2][2]:
+                            # means finger open  #because of open cv pixels it is reverse
+                            fingers_open.append(1)
+                        else:
+
+                            fingers_open.append(0)
+
+                    print(fingers_open)
+
+                    total_fingers_open = fingers_open.count(1)
+
+                    if total_fingers_open==2:
+                        a=False
+
+                if len(lm_list) != 0:
+
+                    # print(lm_list[4],lm_list[8])
+
+                    x1, y1 = lm_list[4][1], lm_list[4][2]
+                    x2, y2 = lm_list[8][1], lm_list[8][2]
+
+                    cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+                    cv2.circle(img, (x1, y1), 10, (255, 0, 255), cv2.FILLED)
+                    # tip of thumb point configuration
+                    cv2.circle(img, (x2, y2), 10, (255, 0, 255), cv2.FILLED)
+
+                    cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+                    # the connection line configuration
+
+                    cv2.circle(img, (cx, cy), 10, (255, 0, 0), cv2.FILLED)
+                    # the midpoint circle configuration
+
+                    length = math.hypot(x2 - x1, y2 - y1)
+                    print(int(length))
+
+                    # vol range -95 to 0
+                    # hand range at a particular distance(will change with distance from the camera of the hand) 300 to 0
+
+                    vol = np.interp(length, [50, 250], [min_vol, max_vol])
+                    print(vol)
+
+                    vol_bar = np.interp(length, [50, 250], [400, 150])
+
+                    vol_percentage = np.interp(length, [50, 250], [0, 100])
+
+                    volume.SetMasterVolumeLevel(vol, None)
+
+                    if length < 50:
+                        cv2.circle(img, (cx, cy), 10, (0, 255, 0), cv2.FILLED)
+
+                cv2.rectangle(img, (50, 150), (85, 400), (0, 255, 0), 3)
+
+                cv2.rectangle(img, (50, int(vol_bar)), (85, 400), (0, 255, 0), cv2.FILLED)
+
+                cv2.putText(img, f'{int(vol_percentage)}%', (40, 450), cv2.FONT_ITALIC, 1, (0, 0, 255), 4)
+
+                cTime = time.time()
+                fps = 1 / (cTime - pTime)
+                pTime = cTime
+                # calculating the frames per second
+
+                cv2.putText(img, f'FPS: {int(fps)}', (10, 30), cv2.FONT_ITALIC, 1, (255, 0, 0), 4)
+                # display the fps setting its font,thickness,color purple,thickness,position,etc.
+
+                cv2.imshow('image', img)
+                # the heading of the webcam video capture
+                cv2.waitKey(1)
+                # setting the delay
+
+        elif 'your name' in question:
+            speak("My name is Spec   and i am your voice assistant.")
+
+        elif 'how are you' in question:
+            speak("I am fine, how are you sir")
+
+        elif 'open youtube' in question:
+            speak('opening...')
+            print('opening...')
+            webbrowser.open("youtube.com")
+
+        elif 'music' in question:
+            speak('opening...')
+            print('opening')
+            webbrowser.open("https://music.youtube.com/")
+
+        elif 'google' in question:
+            speak('opening')
+            print('opening')
+            webbrowser.open("www.google.com")
+
+        elif 'discord' in question:
+            webbrowser.open("https://discord.com/channels/@me")
+            speak("Opening discord")
+            print("opening discord")
+
+        elif 'Gmail' in question:
+            webbrowser.open("https://mail.google.com/mail/u/1/#inbox")
+            speak("opening gmail")
+            print("opening gmail")
+
+        elif "i don't care" in question:
+            webbrowser.open("https://music.youtube.com/watch?v=7hDam9i-Aps&list=RDAMVM7hDam9i-Aps")
+            speak("playing")
+
+        elif "excuses" in question:
+            webbrowser.open("https://music.youtube.com/watch?v=x18b0D8sTwo&list=RDAMVMx18b0D8sTwo")
+            speak("playing")
+
+        elif 'joke' in question:
+            joke=pyjokes.get_joke()
+            speak(joke)
+            print(joke)
+
+        elif 'LMS' in question:
+            webbrowser.open("https://lms.bennett.edu.in/login/index.php")
+            speak("Opening LMS")
+            print("Opening LMS")
+
+
+        elif 'whatsapp' in question:
+            speak('opening')
+            print('opening')
+            webbrowser.open("https://web.whatsapp.com/")
+
+        elif 'wikipedia' in question:
+            speak("opening...")
+            print("opening...")
+            webbrowser.open('https://www.wikipedia.org/')
+
+        elif 'the time' in question:
+            strTime=datetime.datetime.now().strftime("%H:%M:%S")
+            print(strTime)
+            speak(f"Sir, time is {strTime}")
+
+        elif 'pycharm' in question:
+            codepath="D:\\PyCharm Community Edition 2021.2.2\\bin\\pycharm64.exe"
+            os.startfile(codepath)
+            speak("opening...")
+            print("opening...")
+
+        elif 'brave' in question:
+            codepath="C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe"
+            os.startfile(codepath)
+            speak("opening..")
+            print("opening...")
+
+        elif 'android studio' in question:
+            codepath="D:\\android studio\\bin\\studio64.exe"
+            os.startfile(codepath)
+            speak("opening android studio")
+            print("opening android studio")
+
+        elif 'outlook' in question:
+            codepath="C:\\Program Files\\Microsoft Office\\root\\Office16\\OUTLOOK.EXE"
+            os.startfile(codepath)
+            speak("opening outlook")
+            print("opening outlook")
+
+        elif "command" in question:
+            os.system("start cmd")
+            speak("opening command prompt")
+            print("opening command prompt")
+
+        elif "shut down" in question:
+            os.system("shutdown /s /t5")
+
+        elif "sing" in question:
+            speak("Sorry sir, I can't sing but i am sure i few weeks i will learn to sing")
+
+
+        elif "sleep" in question:
+            speak("Thanks for working with me. Bye  Bye sir ,Have a nice day ")
+            sys.exit()
+        else:
+            print("I am still learning")
